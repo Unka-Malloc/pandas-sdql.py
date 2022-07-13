75ad802d86ac3dfe4e9f257b930d083453b6c2e2
Yizhuo Y <cxunka@outlook.com>
2022-07-13 09:54:28 +0100

    2022-07-13

---
+++ tests/test_ColUnit.py
@@ -0,0 +1,22 @@
+import unittest
+from pysdql import Relation
+from pysdql.core.dtypes.ColumnUnit import ColUnit
+
+
+class MyTestCase(unittest.TestCase):
+    @property
+    def relation(self):
+        return Relation('R')
+
+    def test_selection(self):
+        a = self.relation['A']
+
+    def test_get_col(self):
+        a = self.relation['A'] > 1
+
+    def test_not(self):
+        print(~ (self.relation['A'] < 1))
+
+
+if __name__ == '__main__':
+    unittest.main()
