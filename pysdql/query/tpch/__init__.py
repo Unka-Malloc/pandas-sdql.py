d3171315a95d57266244cb9c548437657f4f8705
Yizhuo Y <cxunka@outlook.com>
2023-01-28 02:24:07 +0000

    Now It Looks Beautiful

--- pysdql/query/tpch/__init__.py
+++ pysdql/query/tpch/__init__.py
@@ -6,44 +6,92 @@ import pysdql.query.tpch.Qpandas
 
 from pysdql.query.util import sdql_to_df, pandas_to_df, compare_dataframe
 
-def tpch_query(qindex=1, execution_mode=0, threads_count=1):
+
+def tpch_query(qindex=1, execution_mode=0, threads_count=1) -> bool:
     done = [1, 6]
 
-    if qindex not in done:
-        print(f'Query {qindex} has not been verified.')
-        return
+    if isinstance(qindex, float):
+        if qindex not in done:
+            print(f'Query {qindex} has not been verified.')
+            return False
 
-    sdql_result = eval(f'pysdql.query.tpch.Qsdql.q{qindex}({execution_mode}, {threads_count})')
+        qindex = [qindex]
 
-    pandas_result = eval(f'pysdql.query.tpch.Qpandas.q{qindex}()')
+    if isinstance(qindex, list):
+        check_list = []
+        for q in qindex:
+            if q not in done:
+                print(f'Query {q} has not been verified.')
+                return False
 
-    sdql_df = sdql_to_df(sdql_result)
+            sdql_result = eval(f'pysdql.query.tpch.Qsdql.q{q}({execution_mode}, {threads_count})')
 
-    pandas_df = pandas_to_df(pandas_result)
+            pandas_result = eval(f'pysdql.query.tpch.Qpandas.q{q}()')
 
-    print('<- SDQL ->')
+            sdql_df = sdql_to_df(sdql_result)
 
-    print(sdql_result)
+            pandas_df = pandas_to_df(pandas_result)
 
-    print('<- Pandas ->')
+            print(f'>> Query {q} <<')
 
-    print(pandas_result)
+            print(f'>> SDQL <<')
 
-    if compare_dataframe(sdql_df, pandas_df):
-        print('''
-        ███████╗██╗   ██╗ ██████╗ ██████╗███████╗███████╗███████╗
-        ██╔════╝██║   ██║██╔════╝██╔════╝██╔════╝██╔════╝██╔════╝
-        ███████╗██║   ██║██║     ██║     █████╗  ███████╗███████╗
-        ╚════██║██║   ██║██║     ██║     ██╔══╝  ╚════██║╚════██║
-        ███████║╚██████╔╝╚██████╗╚██████╗███████╗███████║███████║
-        ╚══════╝ ╚═════╝  ╚═════╝ ╚═════╝╚══════╝╚══════╝╚══════╝                                             
-        ''')
+            print(sdql_result)
+
+            print(f'>> Pandas <<')
+
+            print(pandas_result)
+
+            sep_line = '#' * 75
+
+            if compare_dataframe(sdql_df, pandas_df):
+                check_list.append(True)
+                print(sep_line)
+                print(f'''
+                {art_map[q]}
+                ███████╗██╗   ██╗ ██████╗ ██████╗███████╗███████╗███████╗
+                ██╔════╝██║   ██║██╔════╝██╔════╝██╔════╝██╔════╝██╔════╝
+                ███████╗██║   ██║██║     ██║     █████╗  ███████╗███████╗
+                ╚════██║██║   ██║██║     ██║     ██╔══╝  ╚════██║╚════██║
+                ███████║╚██████╔╝╚██████╗╚██████╗███████╗███████║███████║
+                ╚══════╝ ╚═════╝  ╚═════╝ ╚═════╝╚══════╝╚══════╝╚══════╝                                             
+                ''')
+                print(sep_line)
+            else:
+                check_list.append(False)
+                print(sep_line)
+                print(f'''
+                {art_map[q]}
+                ███████╗ █████╗ ██╗██╗     ██╗   ██╗██████╗ ███████╗
+                ██╔════╝██╔══██╗██║██║     ██║   ██║██╔══██╗██╔════╝
+                █████╗  ███████║██║██║     ██║   ██║██████╔╝█████╗  
+                ██╔══╝  ██╔══██║██║██║     ██║   ██║██╔══██╗██╔══╝  
+                ██║     ██║  ██║██║███████╗╚██████╔╝██║  ██║███████╗
+                ╚═╝     ╚═╝  ╚═╝╚═╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝                               
+                ''')
+                print(sep_line)
+        else:
+            return all(check_list)
     else:
-        print('''
-        ███████╗ █████╗ ██╗██╗     ██╗   ██╗██████╗ ███████╗
-        ██╔════╝██╔══██╗██║██║     ██║   ██║██╔══██╗██╔════╝
-        █████╗  ███████║██║██║     ██║   ██║██████╔╝█████╗  
-        ██╔══╝  ██╔══██║██║██║     ██║   ██║██╔══██╗██╔══╝  
-        ██║     ██║  ██║██║███████╗╚██████╔╝██║  ██║███████╗
-        ╚═╝     ╚═╝  ╚═╝╚═╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝                               
-        ''')
+        raise NotImplementedError
+
+
+art_map = {
+    1: '''
+ ██████╗ ██╗   ██╗███████╗██████╗ ██╗   ██╗     ██╗
+██╔═══██╗██║   ██║██╔════╝██╔══██╗╚██╗ ██╔╝    ███║
+██║   ██║██║   ██║█████╗  ██████╔╝ ╚████╔╝     ╚██║
+██║▄▄ ██║██║   ██║██╔══╝  ██╔══██╗  ╚██╔╝       ██║
+╚██████╔╝╚██████╔╝███████╗██║  ██║   ██║        ██║
+ ╚══▀▀═╝  ╚═════╝ ╚══════╝╚═╝  ╚═╝   ╚═╝        ╚═╝
+    ''',
+    6: '''
+ ██████╗ ██╗   ██╗███████╗██████╗ ██╗   ██╗     ██████╗ 
+██╔═══██╗██║   ██║██╔════╝██╔══██╗╚██╗ ██╔╝    ██╔════╝ 
+██║   ██║██║   ██║█████╗  ██████╔╝ ╚████╔╝     ███████╗ 
+██║▄▄ ██║██║   ██║██╔══╝  ██╔══██╗  ╚██╔╝      ██╔═══██╗
+╚██████╔╝╚██████╔╝███████╗██║  ██║   ██║       ╚██████╔╝
+ ╚══▀▀═╝  ╚═════╝ ╚══════╝╚═╝  ╚═╝   ╚═╝        ╚═════╝ 
+
+    ''',
+}
