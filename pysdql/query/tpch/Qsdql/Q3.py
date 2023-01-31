5f0a736ca56296356eea1282f734141bc16d6aef
Yizhuo Y <cxunka@outlook.com>
2023-01-31 04:58:44 +0000

    Verified Compatibility For Q5 and Q8

--- pysdql/query/tpch/Qsdql/Q3.py
+++ pysdql/query/tpch/Qsdql/Q3.py
@@ -9,7 +9,7 @@ def query(li, cu, ord):
     building = "BUILDING"
     customer_part = cu.sum(lambda x_customer: ({x_customer[0].c_custkey: True}) if (x_customer[0].c_mktsegment == building) else (None))
     
-    customer_orders = ord.sum(lambda x_orders: (({x_orders[0].o_orderkey: record({"o_orderdate": x_orders[0].o_orderdate, "o_orderkey": x_orders[0].o_orderkey, "o_shippriority": x_orders[0].o_shippriority})}) if (customer_part[x_orders[0].o_custkey] != None) else (None)) if (x_orders[0].o_orderdate < 19950315) else (None))
+    customer_orders = ord.sum(lambda x_orders: (({x_orders[0].o_orderkey: record({"o_orderdate": x_orders[0].o_orderdate, "o_shippriority": x_orders[0].o_shippriority})}) if (customer_part[x_orders[0].o_custkey] != None) else (None)) if (x_orders[0].o_orderdate < 19950315) else (None))
     
     lineitem_aggr = li.sum(lambda x_lineitem: (({record({"l_orderkey": x_lineitem[0].l_orderkey, "o_orderdate": customer_orders[x_lineitem[0].l_orderkey].o_orderdate, "o_shippriority": customer_orders[x_lineitem[0].l_orderkey].o_shippriority}): ((x_lineitem[0].l_extendedprice) * (((1.0) - (x_lineitem[0].l_discount))))}) if (customer_orders[x_lineitem[0].l_orderkey] != None) else (None)) if (x_lineitem[0].l_shipdate > 19950315) else (None))
     
--- pysdql/query/tpch/Qsdql/Q7.py
+++ pysdql/query/tpch/Qsdql/Q7.py
@@ -10,11 +10,11 @@ def query(su, li, ord, cu, na):
     morocco = "MOROCCO"
     nation_part = na.sum(lambda x_nation: ({x_nation[0].n_nationkey: record({"n_name": x_nation[0].n_name})}) if (((x_nation[0].n_name == peru) + (x_nation[0].n_name == morocco))) else (None))
     
-    nation_supplier = su.sum(lambda x_supplier: ({x_supplier[0].s_suppkey: record({"n1_name": nation_part[x_supplier[0].s_nationkey].n_name, "s_suppkey": x_supplier[0].s_suppkey})}) if (nation_part[x_supplier[0].s_nationkey] != None) else (None))
+    nation_supplier = su.sum(lambda x_supplier: ({x_supplier[0].s_suppkey: record({"n1_name": nation_part[x_supplier[0].s_nationkey].n_name})}) if (nation_part[x_supplier[0].s_nationkey] != None) else (None))
     
-    nation_customer = cu.sum(lambda x_customer: ({x_customer[0].c_custkey: record({"c_custkey": x_customer[0].c_custkey, "n_name": nation_part[x_customer[0].c_nationkey].n_name})}) if (nation_part[x_customer[0].c_nationkey] != None) else (None))
+    nation_customer = cu.sum(lambda x_customer: ({x_customer[0].c_custkey: record({"n_name": nation_part[x_customer[0].c_nationkey].n_name})}) if (nation_part[x_customer[0].c_nationkey] != None) else (None))
     
-    nation_customer_orders = ord.sum(lambda x_orders: ({x_orders[0].o_orderkey: record({"n2_name": nation_customer[x_orders[0].o_custkey].n_name, "o_orderkey": x_orders[0].o_orderkey})}) if (nation_customer[x_orders[0].o_custkey] != None) else (None))
+    nation_customer_orders = ord.sum(lambda x_orders: ({x_orders[0].o_orderkey: record({"n2_name": nation_customer[x_orders[0].o_custkey].n_name})}) if (nation_customer[x_orders[0].o_custkey] != None) else (None))
     
     nation_supplier_nation_customer_orders_lineitem = li.sum(lambda x_lineitem: (((({record({"supp_nation": nation_supplier[x_lineitem[0].l_suppkey].n1_name, "cust_nation": nation_customer_orders[x_lineitem[0].l_orderkey].n2_name, "l_year": extractYear(x_lineitem[0].l_shipdate)}): record({"revenue": ((x_lineitem[0].l_extendedprice) * (((1) - (x_lineitem[0].l_discount))))})}) if (((((nation_supplier[x_lineitem[0].l_suppkey].n1_name == peru) * (nation_customer_orders[x_lineitem[0].l_orderkey].n2_name == morocco))) + (((nation_supplier[x_lineitem[0].l_suppkey].n1_name == morocco) * (nation_customer_orders[x_lineitem[0].l_orderkey].n2_name == peru))))) else (None)) if (nation_supplier[x_lineitem[0].l_suppkey] != None) else (None)) if (nation_customer_orders[x_lineitem[0].l_orderkey] != None) else (None)) if (((x_lineitem[0].l_shipdate >= 19950101) * (x_lineitem[0].l_shipdate <= 19961231))) else (None))
     
