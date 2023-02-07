5284bc611fd892f7028a97aa984616a02ebc72e7
Unka Y.Y <unka.malloc@outlook.com>
2023-02-07 23:48:48 +0000

    Update

--- test_single.py
+++ test_single.py
@@ -3,14 +3,14 @@ import pysdql
 from pysdql.query.tpch.template import *
 
 if __name__ == '__main__':
-    part = pysdql.DataFrame()
-    supplier = pysdql.DataFrame()
-    partsupp = pysdql.DataFrame()
-    nation = pysdql.DataFrame()
-    region = pysdql.DataFrame()
-
-    tpch_q2(part, supplier, partsupp, nation, region).opt_to_sdqlir()
+    # part = pysdql.DataFrame()
+    # supplier = pysdql.DataFrame()
+    # partsupp = pysdql.DataFrame()
+    # nation = pysdql.DataFrame()
+    # region = pysdql.DataFrame()
+    #
+    # tpch_q2(part, supplier, partsupp, nation, region).opt_to_sdqlir()
 
     # test_one = pysdql.tpch_query(2)
 
-    # print(pysdql.query.tpch.Qpandas.q2())
+    print(pysdql.query.tpch.Qpandas.q2())
