74d9dee7aeaac80c8f1f8ad2bcfe3704057c42ed
cxunka <cxunka@outlook.com>
2023-03-11 18:57:38 +0000

    Update test_all.py

--- archive/test_all.py
+++ archive/test_all.py
@@ -1,4 +1,4 @@
 import pysdql
 
 if __name__ == '__main__':
-    test_all = pysdql.tpch_query(4, verbose=False, optimize=True)
+    test_all = pysdql.tpch_query(range(1, 23), verbose=False, optimize=True)
