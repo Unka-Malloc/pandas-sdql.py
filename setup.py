5d5827c1c1153c6fa3e128863e8da2de8de9459a
Unka Y.Y <unka.malloc@outlook.com>
2023-03-11 19:44:25 +0000

    Include toml files

--- setup.py
+++ setup.py
@@ -8,6 +8,11 @@ setup(
     url='',
     description='ts: Test Setup',
     packages=find_packages(exclude=["tests"]),  # 项目中需要拷贝到指定路径的文件夹
+    package_data={
+        '': [
+            '*.toml',
+        ]
+    }
     python_requires='>=3.7.0',
     install_requires=open('requirements.txt').readlines()  # 安装依赖
 )
