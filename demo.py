03aea890234c986527a3ecb8262be3aac747080c
Unka Y.Y <unka.malloc@outlook.com>
2023-03-11 21:11:37 +0000

    Update All

--- demo.py
+++ demo.py
@@ -1,4 +1,5 @@
 import pysdql
 
 if __name__ == '__main__':
-    test_single = pysdql.tpch_query(1, verbose=True, optimize=True)
+    results = pysdql.tpch_query(range(1, 23), verbose=True, optimize=True)
+    
