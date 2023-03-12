3ee489a8549d0077207b19ca8526a2b5dc8a244a
Unka Y.Y <unka.malloc@outlook.com>
2023-03-12 19:12:39 +0000

    Verify [1, 6, 12, 13, 14]

--- demo.py
+++ demo.py
@@ -1,11 +1,8 @@
 import pysdql
 
 if __name__ == '__main__':
-    results = pysdql.tpch_query(range(1, 23), verbose=False, optimize=True)
+    # results = pysdql.tpch_query(range(1, 23), verbose=False, optimize=True)
 
-    # results = pysdql.tpch_query([14, 17], verbose=False, optimize=True)
-
-    # result = pysdql.tpch_query(14, verbose=True, optimize=True, mode='postgres')
+    result = pysdql.tpch_query([1, 6, 12, 13, 14], verbose=True, optimize=True, mode='postgres')
 
     # pysdql.tpch_query(14, verbose=True, optimize=True)
-
