d1686db1767ad3a1d91e402e8941578d9a48a591
Yizhuo Y <cxunka@outlook.com>
2023-03-14 13:47:03 +0000

    Update test_pd2sd.py

--- test_pd2sd.py
+++ test_pd2sd.py
@@ -305,7 +305,7 @@ def q8(pa, su, li, ord, cu, na, re):
     n2_supplier_part_region_n1_customer_orders_lineitem = li.sum(lambda x_lineitem: (((((((((((({
         extractYear(region_n1_customer_orders[x_lineitem[0].l_orderkey].o_orderdate): record({"A": (
         ((x_lineitem[0].l_extendedprice) * (((1) - (x_lineitem[0].l_discount))))) if (
-                    n2_part[supplier_part[x_lineitem[0].l_suppkey].s_nationkey].n2_name == brazil) else (0.0), "B": (
+                    n2_part[supplier_part[x_lineitem[0].l_suppkey].s_nationkey].n_name == brazil) else (0.0), "B": (
                     (x_lineitem[0].l_extendedprice) * (((1) - (x_lineitem[0].l_discount))))})}) if (
     n2_part[supplier_part[x_lineitem[0].l_suppkey].s_nationkey]) else (None)) if (
     supplier_part[x_lineitem[0].l_suppkey]) else (None)) if (part_part[x_lineitem[0].l_partkey]) else (None)) if (
