e3415523df0ab4f8c40509eb09e007eaa85df2c6
Unka Y.Y <unka.malloc@outlook.com>
2023-03-31 09:07:59 +0100

    submit

--- pysdql/query/tpch/Qpostgres/Q22.py
+++ pysdql/query/tpch/Qpostgres/Q22.py
@@ -13,6 +13,10 @@ def query(cu, cu1, ord):
     v30 = "30"
     v18 = "18"
     v17 = "17"
+    orders_0 = ord.sum(lambda x: {x[0]: {record({"o_custkey": x[0].o_custkey}): True}})
+    
+    orders_customer_build_pre_ops = orders_0.sum(lambda x: x[1])
+    
     customer_0 = cu.sum(lambda x: ({x[0]: x[1]}) if (((x[0].c_acctbal > 0.0) * (((((((((((((substr(x[0].c_phone, 0, 1) == v17) + (substr(x[0].c_phone, 0, 1) == v18))) + (substr(x[0].c_phone, 0, 1) == v13))) + (substr(x[0].c_phone, 0, 1) == v31))) + (substr(x[0].c_phone, 0, 1) == v23))) + (substr(x[0].c_phone, 0, 1) == v29))) + (substr(x[0].c_phone, 0, 1) == v30))))) else (None))
     
     customer_1 = customer_0.sum(lambda x: (record({"c_acctbal_sum_for_mean": x[0].c_acctbal, "c_acctbal_count_for_mean": 1.0})) if (x[0].c_acctbal != None) else (0.0))
@@ -20,7 +24,7 @@ def query(cu, cu1, ord):
     avgc_acctbal_el_0_c_acctbal_mean = ((customer_1.c_acctbal_sum_for_mean) / (customer_1.c_acctbal_count_for_mean))
     orders_customer_probe_pre_ops = cu.sum(lambda x: ({x[0]: x[1]}) if (((x[0].c_acctbal > avgc_acctbal_el_0_c_acctbal_mean) * (((((((((((((substr(x[0].c_phone, 0, 1) == v17) + (substr(x[0].c_phone, 0, 1) == v18))) + (substr(x[0].c_phone, 0, 1) == v13))) + (substr(x[0].c_phone, 0, 1) == v31))) + (substr(x[0].c_phone, 0, 1) == v23))) + (substr(x[0].c_phone, 0, 1) == v29))) + (substr(x[0].c_phone, 0, 1) == v30))))) else (None))
     
-    customer_orders_build_nest_dict = ord.sum(lambda x: {x[0].o_custkey: sr_dict({x[0]: x[1]})})
+    customer_orders_build_nest_dict = orders_customer_build_pre_ops.sum(lambda x: {x[0].o_custkey: sr_dict({x[0]: x[1]})})
     
     customer_orders_0 = orders_customer_probe_pre_ops.sum(lambda x: ({x[0]: True}) if (customer_orders_build_nest_dict[x[0].c_custkey] == None) else (None))
     
